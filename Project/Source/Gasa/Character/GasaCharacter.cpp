99a3e46635f624a992ffea3572ec944c4d93262b
Ed_ <edwardgz@gmail.com>
2024-04-12 19:55:34 -0400

    Game Mode, Camera

--- Project/Source/Gasa/Character/GasaCharacter.cpp
+++ Project/Source/Gasa/Character/GasaCharacter.cpp
@@ -1,11 +1,41 @@
 #include "GasaCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 AGasaCharacter::AGasaCharacter()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	UCharacterMovementComponent*
+	Movement = GetCharacterMovement();
+	Movement->bOrientRotationToMovement = true;
+	Movement->bConstrainToPlane         = true;
+	Movement->bSnapToPlaneAtStart       = true;
+	Movement->RotationRate              = FRotator(0.0, 400.f, 0.0);
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll  = false;
+	bUseControllerRotationYaw   = false;
+
+	USceneComponent*        root_component = GetRootComponent();
+	USkeletalMeshComponent* mesh           = GetMesh();
+
+	CamSpringArm = CreateDefaultSubobject<USpringArmComponent>("Camera Spring Arm");
+	CamSpringArm->SetupAttachment(root_component);
+	CamSpringArm->SetRelativeRotation( FQuat::MakeFromEuler(FVector(0.0, -35.0, 0.0)));
+	CamSpringArm->TargetArmLength = 400.0f;
+
+	CamSpringArm->bInheritPitch = false;
+	CamSpringArm->bInheritYaw   = false;
+	CamSpringArm->bInheritRoll  = false;
+	
+	Camera = CreateDefaultSubobject<UCameraComponent>("Camera");
+	Camera->SetupAttachment(CamSpringArm);
+
 	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
-	Weapon->SetupAttachment(GetMesh(), FName("WeaponAttach"));
+	Weapon->SetupAttachment(mesh, FName("WeaponAttach"));
 	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
---
+++ Project/Source/Gasa/GasaGameMode.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "GameFramework/GameMode.h"
+
+#include "GasaGameMode.generated.h"
+
+UCLASS(Blueprintable)
+class GASA_API AGasaGameMode : public AGameMode
+{
+	GENERATED_BODY()
+public:
+};
