b886b91bd00565ecd6ccaa51a1cb7a7b83726be7
Ed_ <edwardgz@gmail.com>
2024-04-12 14:49:22 -0400

    Character blueprint setup

---
+++ Project/Source/Gasa/Character/PlayerCharacter.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+#include "GasaCharacter.h"
+
+#include "PlayerCharacter.generated.h"
+
+UCLASS(Blueprintable)
+class GASA_API APlayerCharacter : public AGasaCharacter
+{
+	GENERATED_BODY()
+public:
+
+	
+};
