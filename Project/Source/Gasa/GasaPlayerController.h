9fcb7329a3e99caa5a5668249946618700ea23e1
Ed_ <edwardgz@gmail.com>
2024-04-12 16:10:06 -0400

    Enhanced Input

---
+++ Project/Source/Gasa/GasaPlayerController.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "GasaPlayerController.generated.h"
+
+UCLASS(Blueprintable)
+class GASA_API AGasaPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	
+};
